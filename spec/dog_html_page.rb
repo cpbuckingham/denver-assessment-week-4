d3acff16cccbd1dfa7f34257c0219d5c1dbf973b
Cameron <cameron.p.buckingham@gmail.com>
2014-06-30 12:02:46 -0600

    figured it out

--- spec/dog_html_page.rb
+++ spec/dog_html_page.rb
@@ -5,26 +5,23 @@ class DogHTMLPage
   end
 
   def title
-    p "<h1>There are #{@dog_array.length.to_s} dogs</h1>"
-  end
-
-  def title
     if @dog_array.length > 0
-      "<h1>There are #{@dog_array.length.to_s} dogs</h1>"
+      p "<h1>There are #{@dog_array.length.to_s} dogs</h1>"
     else
-      "<h1>There are no dogs</h1>"
+     p "<h1>There are no dogs</h1>"
     end
+end
 
-    def list
-      if @dog_array.length == 0 then p ""
-       else
-         list_string = "<ul>"
-         @dog_array.each do | dog |
-         list_string += "<li>#{dog[:name]}</li>"
-         end
-         p list_string + "</ul>"
+  def list
+    if @dog_array.length == 0 then p ""
+    else
+      list_string = "<ul>"
+      @dog_array.each do | dog |
+        list_string += "<li>#{dog[:name]}</li>"
+      end
+      p list_string + "</ul>"
     end
-  end
   end
 end
+
 
